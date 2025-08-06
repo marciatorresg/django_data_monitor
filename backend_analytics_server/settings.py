34507835c4287491aa598804ac655151bf70efd7
marciatorresg <marciatorresg@gmail.com>
2025-08-06 11:32:39 -0500

    Guia 23 terminada

--- backend_analytics_server/settings.py
+++ backend_analytics_server/settings.py
@@ -132,4 +132,4 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-API_URL = 'https://jsonplaceholder.typicode.com/posts'
+API_URL = 'http://mtorresg.pythonanywhere.com/landing/api/index/?format=json'
